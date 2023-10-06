1c876a3e49f08a6f456287dd5eada9ea7d9dd9b0
Fate <dxfinitelynotfate@gmail.com>
2023-10-06 23:13:36 +0300

    Tags adding js fixes

--- js/addingTags.js
+++ js/addingTags.js
@@ -4,16 +4,24 @@ const button = document.getElementsByClassName('btn-default')[0]
 
 button.addEventListener('click', (e) => {
     e.preventDefault() 
+
+    const array = Array.from(tags.getElementsByTagName("li")).map((item) => item.innerText.slice(0, -1))
     
     const tag = document.createElement('li') 
     const tagContent = input.value.trim() 
-      
+
     if (tagContent !== '') { 
+        if(array.includes(tagContent)) {
+            return
+        }
+
         tag.innerText = tagContent
         tag.innerHTML += '<button class="delete-btn">X</button>'
         tags.appendChild(tag) 
 
         input.value = ''
+
+        console.log(array)
     }
 })
 
